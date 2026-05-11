2063de402dec5f2ee1f36dd44bac683263a336e4
user1 <user1@deb13.2>
2026-05-11 15:40:21 +0300

    initial app - first steps!

---
+++ src/main/java/my/steam/authgate/AuthgateApplication.java
@@ -0,0 +1,13 @@
+package my.steam.authgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/steam/authgate/AuthgateApplicationTests.java
@@ -0,0 +1,13 @@
+package my.steam.authgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
