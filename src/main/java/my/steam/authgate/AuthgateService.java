0b281bceba6cade1f0e0eedb81d24675d30843fd
neonthought <jook200200@gmail.com>
2026-05-17 12:25:12 +0300

    добавляю проверку почты

--- src/main/java/my/steam/authgate/AuthgateService.java
+++ src/main/java/my/steam/authgate/AuthgateService.java
@@ -3,9 +3,13 @@ import my.steam.authgate.UserSMA;
 import my.steam.authgate.AuthgateRepository;
 import org.springframework.stereotype.Service; 
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import java.util.UUID;
 
 @Service
 public class AuthgateService {
@@ -21,18 +25,39 @@ public class AuthgateService {
     } 
 
 
+    // Отправить письмо для подтверждения email
+    public void sendVerificationEmail(String recipientEmail, String token) {
+        
+        String confirmationUrl = "http://localhost:8081/auth/verify?token=" + token;
+        
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(recipientEmail);
+        message.setSubject("Подтверждение регистрации");
+        message.setText("Перейдите по ссылке для подтверждения аккаунта: " + confirmationUrl);
+        
+        mailSender.send(message);
+    }
 
-    // Зарегистрироваться
-    // 0 - успешно создан, 1 - пользователь уже существует
-    public int signup(String username, String password){
+    // Зарегистрировать пользователя
+    // 0 - успешно создан, 
+    // 1 - пользователь уже существует
+    // 2 - email уже используется
+    public int signup(String username, String password, String email){
         
         if (userExists(username)) {
             result = 1;
-        } else {
+        } 
+        else if (emailExists(email)) {
+            result = 2;
+        }
+        else {
  
             //шифруем пароль
             password = passwordEncoder.encode(password);
-            UserSMA user = new UserSMA(username, password);
+
+            // отправляем письмо для подтверждения email
+
+            UserSMA user = new UserSMA(username, password, email);
 
             userRepository.save(user);
 
@@ -45,6 +70,10 @@ public class AuthgateService {
 
     public boolean userExists(String username) {
         return userRepository.findByUsername(username).isPresent();
+    }
+
+    public boolean emailExists(String email) {
+        return userRepository.findByEmail(email).isPresent();
     }
 
 
--- src/main/java/my/steam/authgate/UserSMA.java
+++ src/main/java/my/steam/authgate/UserSMA.java
@@ -15,7 +15,7 @@ import jakarta.persistence.Column;
 
 @Entity
 @Table(name="users", schema = "auth")
-public class UserSMA{
+public class UserSMA{ // user of steam market analisys system 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,11 +28,22 @@ public class UserSMA{
     @Column(name = "password")
     String password; 
 
+    @Column(name = "email")
+    String email; 
+
+    @Column(name = "is_active")
+    boolean isActive;
+
+    //используется для подтверждения email при регистрации (uuid). Испоьзовать для разных целей связанных с токеном
+    @Column(name = "token");
+    String token;
+
     public UserSMA() {}
 
-    public UserSMA(String username, String password) {
+    public UserSMA(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     String getId() {
@@ -58,4 +69,29 @@ public class UserSMA{
     void setPassword( String password) {
         this.password = password;
     }
+
+    String getEmail() {
+        return this.email;
+    }
+
+    void setEmail(String email) {
+        this.email = email;
+    }
+
+        String getToken() {
+        return this.token;
+    }
+
+    void setToken(String token) {
+        this.token = token;
+    }
+
+    String getIsActive() {
+        return this.token;
+    }
+
+    void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
 }
